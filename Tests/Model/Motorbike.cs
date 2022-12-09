d7d85a3407fc1ec3d8461ba36e497acbea7d099b
KaShTaN27 <dkachan.lida@mail.ru>
2022-12-10 00:01:31 +0300

    Add test model

---
+++ Tests/Model/Motorbike.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests.Model; 
+
+public class Motorbike {
+
+    public string Model { get; }
+    private int engineVolume;
+
+    public Motorbike(string model, int engineVolume) {
+        this.engineVolume = engineVolume;
+        Model = model;
+    }
+}
